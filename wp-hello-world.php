0d393034f4f5029f96b646d448763455953ee575
Adam Mazurkiewicz-Madej <adam.mazurkiewicz@ventum.com>
2024-02-19 13:38:50 +0100

    fix pipe file

--- wp-hello-world.php
+++ wp-hello-world.php
@@ -8,8 +8,123 @@
 	 * License: 
 	 */
 	
+	
+	/* EXAMPLE RESPONSE:
+	
+	{
+	  "url": "https://api.github.com/repos/raremont/rare-wp-updater/releases/142500951",
+	  "assets_url": "https://api.github.com/repos/raremont/rare-wp-updater/releases/142500951/assets",
+	  "upload_url": "https://uploads.github.com/repos/raremont/rare-wp-updater/releases/142500951/assets{?name,label}",
+	  "html_url": "https://github.com/raremont/rare-wp-updater/releases/tag/v0.0.3",
+	  "id": 142500951,
+	  "author": {
+		 "login": "github-actions[bot]",
+		 "id": 41898282,
+		 "node_id": "MDM6Qm90NDE4OTgyODI=",
+		 "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+		 "gravatar_id": "",
+		 "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+		 "html_url": "https://github.com/apps/github-actions",
+		 "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+		 "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+		 "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+		 "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+		 "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+		 "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+		 "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+		 "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+		 "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+		 "type": "Bot",
+		 "site_admin": false
+	  },
+	  "node_id": "RE_kwDOLUQJlM4IfmRX",
+	  "tag_name": "v0.0.3",
+	  "target_commitish": "master",
+	  "name": "v0.0.3",
+	  "draft": false,
+	  "prerelease": false,
+	  "created_at": "2024-02-18T15:37:37Z",
+	  "published_at": "2024-02-18T15:38:06Z",
+	  "assets": [
+		 {
+			"url": "https://api.github.com/repos/raremont/rare-wp-updater/releases/assets/152263256",
+			"id": 152263256,
+			"node_id": "RA_kwDOLUQJlM4JE1pY",
+			"name": "rare-wp-updater_v0.0.3.zip",
+			"label": "",
+			"uploader": {
+			  "login": "github-actions[bot]",
+			  "id": 41898282,
+			  "node_id": "MDM6Qm90NDE4OTgyODI=",
+			  "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+			  "gravatar_id": "",
+			  "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+			  "html_url": "https://github.com/apps/github-actions",
+			  "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+			  "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+			  "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+			  "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+			  "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+			  "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+			  "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+			  "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+			  "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+			  "type": "Bot",
+			  "site_admin": false
+			},
+			"content_type": "raw",
+			"state": "uploaded",
+			"size": 11376,
+			"download_count": 0,
+			"created_at": "2024-02-18T15:38:07Z",
+			"updated_at": "2024-02-18T15:38:07Z",
+			"browser_download_url": "https://github.com/raremont/rare-wp-updater/releases/download/v0.0.3/rare-wp-updater_v0.0.3.zip"
+		 },
+		 {
+			"url": "https://api.github.com/repos/raremont/rare-wp-updater/releases/assets/152263258",
+			"id": 152263258,
+			"node_id": "RA_kwDOLUQJlM4JE1pa",
+			"name": "wp-update.json",
+			"label": "",
+			"uploader": {
+			  "login": "github-actions[bot]",
+			  "id": 41898282,
+			  "node_id": "MDM6Qm90NDE4OTgyODI=",
+			  "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+			  "gravatar_id": "",
+			  "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+			  "html_url": "https://github.com/apps/github-actions",
+			  "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+			  "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+			  "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+			  "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+			  "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+			  "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+			  "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+			  "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+			  "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+			  "type": "Bot",
+			  "site_admin": false
+			},
+			"content_type": "raw",
+			"state": "uploaded",
+			"size": 1336,
+			"download_count": 1,
+			"created_at": "2024-02-18T15:38:07Z",
+			"updated_at": "2024-02-18T15:38:08Z",
+			"browser_download_url": "https://github.com/raremont/rare-wp-updater/releases/download/v0.0.3/wp-update.json"
+		 }
+	  ],
+	  "tarball_url": "https://api.github.com/repos/raremont/rare-wp-updater/tarball/v0.0.3",
+	  "zipball_url": "https://api.github.com/repos/raremont/rare-wp-updater/zipball/v0.0.3",
+	  "body": ""
+	}
+	 
+	 *
+	 * */
+	
 	defined('ABSPATH') || exit;
-
+	
 	
 	if (!class_exists('rareUpdateChecker')) {
 		
@@ -33,6 +148,52 @@
 				
 			}
 			
+			private function findAsset ($assets, $name, $matchMode = 'exact') {
+				
+				foreach ($assets as $asset) {
+					
+					if (
+						'exact' === $matchMode
+						&& $asset->name === $name
+					) {
+						return $asset;
+					}
+					
+					if (
+						'contains' === $matchMode
+						&& false !== strpos($asset->name, $name)
+					) {
+						return $asset;
+					}
+					
+				}
+				
+				return false;
+			}
+			
+			private function getMetadata ($remote) {
+				
+				$meta = $this->findAsset($remote->assets, 'wp-data.json');
+				
+				if ($meta) {
+					$meta = wp_remote_get($meta->browser_download_url);
+					$meta = json_decode(wp_remote_retrieve_body($meta));
+				}
+				
+				return $meta;
+			}
+			
+			private function getZipUrl ($remote) {
+				
+				$zip = $this->findAsset($remote->assets, '.zip', 'contains');
+				
+				if ($zip) {
+					return $zip->browser_download_url;
+				}
+				
+				return false;
+			}
+			
 			public function request () {
 				
 				$remote = get_transient($this->cache_key);
@@ -40,12 +201,12 @@
 				if (false === $remote || !$this->cache_allowed) {
 					
 					$remote = wp_remote_get(
-						'https://api.github.com/repos/rarest-adam/wp-hello-world/releases/latest',
+						'https://api.github.com/repos/raremont/rare-wp-updater/releases/latest',
 						array(
 							'timeout' => 10,
 							'headers' => array(
-								'Accept' => 'application/json',
-								'Authorization' => 'Bearer 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'
+								'Accept'        => 'application/json',
+//								'Authorization' => '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'
 							)
 						)
 					);
@@ -93,39 +254,47 @@
 					return $res;
 				}
 				
+				// Get wp-data.json
+				// Find the assets with the name wp-data.json
+				
+				
+				$meta = $this->getMetadata($remote);
+				
+				/* META Example */
+				
 				$res = new stdClass();
+
+//				$res->slug        = $remote->tag_name;
+//				$res->plugin      = plugin_basename(__FILE__); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
+//				$res->new_version = $remote->tag_name;
+////			$res->tested                       = $remote->tested;
+//				$res->package = $remote->assets[0]->browser_download_url;;
 				
-				$res->slug                         = $remote->tag_name;
-				$res->plugin                       = plugin_basename(__FILE__); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
-				$res->new_version                  = $remote->tag_name;
-//			$res->tested                       = $remote->tested;
-				$res->package                      = $remote->assets[0]->browser_download_url;;
 				
+				$res->name           = $meta->name;
+				$res->slug           = $meta->slug;
+				$res->version        = $meta->version;
+				$res->tested         = $meta->tested;
+				$res->requires       = $meta->requires;
+				$res->author         = $meta->author;
+				$res->author_profile = $meta->author_profile;
+				$res->download_link  = $this->getZipUrl($remote);
+				$res->trunk          = $this->getZipUrl($remote);
+				$res->requires_php   = $meta->requires_php;
+				$res->last_updated   = $meta->last_updated;
 				
-//				$res->name    = $remote->tag_name;
-//				$res->slug    = $remote->tag_name;
-//				$res->version = $remote->tag_name;
-//				$res->tested         = $remote->tested;
-//				$res->requires       = $remote->requires;
-//				$res->author         = $remote->author;
-//				$res->author_profile = $remote->author_profile;
-//				$res->download_link = $remote->assets[0]->browser_download_url;
-//				$res->trunk         = $remote->assets[0]->browser_download_url;
-//				$res->requires_php   = $remote->requires_php;
-//				$res->last_updated = $remote->published_at;
+				$res->sections = array(
+					'description'  => $meta->sections->description,
+					'installation' => $meta->sections->installation,
+					'changelog'    => $meta->sections->changelog
+				);
 				
-//				$res->sections = array(
-//					'description'  => $remote->sections->description,
-//					'installation' => $remote->sections->installation,
-//					'changelog'    => $remote->sections->changelog
-//				);
-//
-//				if (!empty($remote->banners)) {
-//					$res->banners = array(
-//						'low'  => $remote->banners->low,
-//						'high' => $remote->banners->high
-//					);
-//				}
+				if (!empty($meta->banners)) {
+					$res->banners = array(
+						'low'  => $meta->banners->low,
+						'high' => $meta->banners->high
+					);
+				}
 				
 				return $res;
 				
@@ -139,6 +308,9 @@
 				
 				$remote = $this->request();
 				
+				$meta = $this->findAsset($remote->assets, 'wp-data.json');
+				$zip  = $this->findAsset($remote->assets, '.zip', 'contains');
+
 //				if (
 //					$remote
 //					&& version_compare($this->version, $remote->version, '<')
@@ -147,12 +319,12 @@
 //				)
 				{
 					
-					$res                               = new stdClass();
-					$res->slug                         = $remote->tag_name;
-					$res->plugin                       = plugin_basename(__FILE__); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
-					$res->new_version                  = $remote->tag_name;
+					$res              = new stdClass();
+					$res->slug        = $remote->tag_name;
+					$res->plugin      = plugin_basename(__FILE__); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
+					$res->new_version = $remote->tag_name;
 //			$res->tested                       = $remote->tested;
-					$res->package                      = $remote->assets[0]->browser_download_url;;
+					$res->package = $this->getZipUrl($remote);
 					$transient->response[$res->plugin] = $res;
 					
 					//$transient->checked[$res->plugin] = $remote->version;
@@ -181,64 +353,64 @@
 		new rareUpdateChecker();
 		
 	}
-	
-	add_filter('site_transient_update_plugins', 'rare_push_update');
-	
-	function rare_push_update ($transient) {
-
-		if (empty($transient->checked)) {
-			return $transient;
-		}
-		
-		$remote = wp_remote_get(
-			'https://api.github.com/repos/rarest-adam/wp-hello-world/releases/latest',
-			array(
-				'timeout' => 10,
-				'headers' => array(
-					'Accept' => 'application/json',
-					'Authorization' => 'Bearer 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'
-				)
-			)
-		);
-		
-		error_log('gettting remote');
-		
-		error_log('remote: ' . print_r($remote, true));
-		
-		if (
-			is_wp_error($remote)
-			|| 200 !== wp_remote_retrieve_response_code($remote)
-			|| empty(wp_remote_retrieve_body($remote))
-			) {
-			error_log('Error getting remote...');
-			return $transient;
-		}
-		
-		$remote = json_decode(wp_remote_retrieve_body($remote));
-		
-		error_log('tag_name: ' . $remote->tag_name);
-		error_log('url: ' . $remote->assets[0]->browser_download_url);
-		
-		// your installed plugin version should be on the line below! You can obtain it dynamically of course
+//
+//	add_filter('site_transient_update_plugins', 'rare_push_update');
+//
+//	function rare_push_update ($transient) {
+//
+//		if (empty($transient->checked)) {
+//			return $transient;
+//		}
+//
+//		$remote = wp_remote_get(
+//			'https://api.github.com/repos/rarest-adam/wp-hello-world/releases/latest',
+//			array(
+//				'timeout' => 10,
+//				'headers' => array(
+//					'Accept'        => 'application/json',
+//					'Authorization' => 'Bearer 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'
+//				)
+//			)
+//		);
+//
+//		error_log('gettting remote');
+//
+//		error_log('remote: ' . print_r($remote, true));
+//
 //		if (
-//			$remote
-//			&& version_compare($this->version, $remote->version, '<')
-//			&& version_compare($remote->requires, get_bloginfo('version'), '<')
-//			&& version_compare($remote->requires_php, PHP_VERSION, '<')
-//		)
- 		{
-			
-			$res                               = new stdClass();
-			$res->slug                         = $remote->tag_name;
-			$res->plugin                       = plugin_basename(__FILE__); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
-			$res->new_version                  = $remote->tag_name;
-//			$res->tested                       = $remote->tested;
-			$res->package                      = $remote->assets[0]->browser_download_url;;
-			$transient->response[$res->plugin] = $res;
-			
-			//$transient->checked[$res->plugin] = $remote->version;
-		}
-		
-		return $transient;
-		
-	}
+//			is_wp_error($remote)
+//			|| 200 !== wp_remote_retrieve_response_code($remote)
+//			|| empty(wp_remote_retrieve_body($remote))
+//		) {
+//			error_log('Error getting remote...');
+//			return $transient;
+//		}
+//
+//		$remote = json_decode(wp_remote_retrieve_body($remote));
+//
+//		error_log('tag_name: ' . $remote->tag_name);
+//		error_log('url: ' . $remote->assets[0]->browser_download_url);
+//
+//		// your installed plugin version should be on the line below! You can obtain it dynamically of course
+////		if (
+////			$remote
+////			&& version_compare($this->version, $remote->version, '<')
+////			&& version_compare($remote->requires, get_bloginfo('version'), '<')
+////			&& version_compare($remote->requires_php, PHP_VERSION, '<')
+////		)
+//		{
+//
+//			$res              = new stdClass();
+//			$res->slug        = $remote->tag_name;
+//			$res->plugin      = plugin_basename(__FILE__); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
+//			$res->new_version = $remote->tag_name;
+////			$res->tested                       = $remote->tested;
+//			$res->package = $remote->assets[0]->browser_download_url;;
+//			$transient->response[$res->plugin] = $res;
+//
+//			//$transient->checked[$res->plugin] = $remote->version;
+//		}
+//
+//		return $transient;
+//
+//	}
